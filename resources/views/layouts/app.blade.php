4fa965d763c6af653530f31d5d7fe87a83721f7d
Janrey Maligro <malricjan25@mail.com>
2019-11-14 17:37:24 +0800

    bug

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -77,7 +77,8 @@
             @yield('content')
         </main>
     </div>
-
+    <script src="{{ asset('assets/js/vendors/jquery-3.2.1.min.js') }}"></script>
+    <script src="{{ asset('assets/js/vendors/bootstrap.bundle.min.js') }}"></script>
     <script src="{{ asset('assets/plugins/datatable/jquery.dataTables.min.js') }}"></script>
     <script src="{{ asset('assets/plugins/datatable/dataTables.bootstrap4.min.js') }}"></script>
 
