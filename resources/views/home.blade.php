b26795b5bcd0978871f5935fb43ea57f9e32531d
Janrey Maligro <malricjan25@mail.com>
2019-11-14 17:31:02 +0800

    datatable

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -30,7 +30,7 @@
                                 <h3 class="card-title">Basic Table</h3>
                             </div>
                             <div class="table-responsive">
-                                <table class="table card-table table-vcenter text-nowrap">
+                                <table id="datatable" class="table card-table table-vcenter text-nowrap">
                                     <thead>
                                         <tr>
                                             <th>ID</th>
@@ -68,3 +68,16 @@
     </div>
 </div>
 @endsection
+@push('additionalCSS')
+<link href="{{ asset('assets/plugins/datatable/dataTables.bootstrap4.min.css') }}" rel="stylesheet" />
+@endpush
+@push('additionalJS')
+<script src="{{ asset('assets/plugins/datatable/jquery.dataTables.min.js') }}"></script>
+<script src="{{ asset('assets/plugins/datatable/dataTables.bootstrap4.min.js') }}"></script>
+
+<script>
+    $(function(e) {
+        $('#datatable').DataTable();
+    } );
+</script>
+@endpush
